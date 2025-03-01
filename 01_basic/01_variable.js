560058174d590e88c666827223f8260b960b7759
fatima643 <fatimasehar912@gmail.com>
2025-03-01 12:13:23 +0000

    01_variables&constant

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,17 @@
+const accountId=123898
+let accountEmail= "fatima@gmail.com"
+var accounPassword="128736"
+ accountcity="faisalbad"
+
+//   accountId=233 not allowed
+
+ accountPassword="2927"
+ accountEmail="isha@gmail"
+ accountcity="lahore"
+ let accountState;
+console.table([ accountEmail,accountcity,accountPassword, accountState]);
+
+/*
+prefer not use var
+because of block scope issue
+*/
