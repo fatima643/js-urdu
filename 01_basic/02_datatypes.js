802461ead8800f03f9fbe6be8a3104a4e0439283
fatima643 <fatimasehar912@gmail.com>
2025-03-03 07:55:21 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all JS newer version
+
+// alert(3+3)// we are using nodejs,not browser that's why its not run we use another syntax
+/*
+console.log(3
+    +3
+); // code must be readability
+
+
+let="fatima" //string 
+let age= 18 //Number
+let userLogedIn= true // boolean 
+*/
+/*
+premitive datatype
+number=> range(2 to power 53)
+bigInt=> use large data value
+string=>""
+boolean=> true/false
+ null=> standlone value
+ undefined=> 
+symbol=> unique
+
+*/
+
+//object
+
+console.log(typeof "fatima");
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
