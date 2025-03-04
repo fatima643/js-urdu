c79420abb1667630a165941434ea483f5fabb8c1
fatima643 <fatimasehar912@gmail.com>
2025-03-04 07:41:21 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score= "33abc"
+
+console.log(typeof score);
+ 
+let valueIn= Number(score)
+console.log(typeof valueIn)
+console.log(valueIn)
+
+
+/*
+"33" => 33 
+"33abc"=> NaN
+true=> 1, false=> 0
+
+*/
+
+let isLoggedIn= 1   // data type is true
+
+let LoggedIn= 0 // data type is false
+
+let Logged= "" // data type is false
+ 
+let userLogged="fatima"  // data type is true
+let  booleanIsLoged= Boolean(isLoggedIn)
+
+console.log(booleanIsLogeds)
+
+let someNumber=44
+
+let stringNumber= String(someNumber)
+
+console.log(stringNumber); //it convert into 44 
+console.log(typeof stringNumber); // its string
+
+
+
+
