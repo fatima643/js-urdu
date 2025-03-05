8ab4de2a2a8a8580199ee7975e9f93aeb9d585e1
fatima643 <fatimasehar912@gmail.com>
2025-03-05 07:40:01 +0000

    operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score= "33abc"
 
-console.log(typeof score);
+// console.log(typeof score);
  
 let valueIn= Number(score)
-console.log(typeof valueIn)
-console.log(valueIn)
+// console.log(typeof valueIn)
+// console.log(valueIn)
 
 
 /*
@@ -21,16 +21,85 @@ let LoggedIn= 0 // data type is false
 let Logged= "" // data type is false
  
 let userLogged="fatima"  // data type is true
-let  booleanIsLoged= Boolean(isLoggedIn)
+let booleanLoged= Boolean(isLoggedIn)
 
-console.log(booleanIsLogeds)
+// console.log(booleanLoged)
 
 let someNumber=44
 
 let stringNumber= String(someNumber)
 
-console.log(stringNumber); //it convert into 44 
-console.log(typeof stringNumber); // its string
+// console.log(stringNumber); //it convert into 44 
+// console.log(typeof stringNumber); // its string
+
+
+
+//  ******************************Operations***************************//
+
+let value= 4
+let negvalue= -value
+
+// console.log(negvalue);
+
+let str1= "hello"
+let str2=" fatima"
+
+let str3= str1+str2
+// console.log(str3);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// console.log(1+2);
+// console.log(2+1);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(true);
+
+// console.log(+true);
+// console.log(+"");
+// console.log(+undefined);
+
+let num1,num2,num3
+ num1=num2=num3=2+2
+//  console.log(num1,num2,num3);
+
+let gamecounter=100
+
+++gamecounter;
+// console.log(gamecounter);
+// console.log(gamecounter);
+
+// ******************Postfix increment***************************
+
+let x=3
+// x++;
+const y= x++
+
+
+let x2 = 3n;
+const y2 = x2++;
+
+console.log(x,y);
+console.log(x2,y2);
+// *******************Prefix increment***************8
+let a = 3;
+const b = ++a;
+
+
+let a2 = 3n;
+const b2 = ++a2;
+console.log(a,b);
+console.log(a2,b2);
+ 
+
 
 
 
