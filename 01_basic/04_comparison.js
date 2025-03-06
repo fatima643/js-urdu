291a05b703b6849537e7e8b8e086401082af4284
fatima643 <fatimasehar912@gmail.com>
2025-03-06 07:31:53 +0000

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,42 @@
+// console.log(2> 2);
+// console.log(2< 2);
+// console.log(2<= 2);
+// console.log(2>= 2);
+// console.log(2!=2);
+// console.log(2==2);
+
+
+// console.log("2"> 1);
+// console.log("02" > 1);
+
+
+
+// console.log(null > 0);
+// console.log(null <0 );
+// console.log(null == 0);
+// console.log(null>=0);
+// console.log(null<=0);
+
+
+
+
+
+// the reason that an equality check== and comparison > 
+// < >= <= work differently
+// comparison convert  null to a number that's why line 17 and 18 is true and line 14,15,16 false
+
+// console.log(undefined > 0);
+// console.log(undefined <0 );
+// console.log(undefined == 0);
+// console.log(undefined>=0);
+// console.log(undefined<=0);
+// note: null or undefined wly comparison sy avoid kena chai
+
+
+//==== strictly check ye dekhy ga k dono trf same datatype 
+// ha k nahi mujy comparsion karna chai ka nhi lakin == ye disfferrent work karta ha 
+// ye comparsion karna start kr deta hai
+
+console.log("2"===2);
+console.log("2"==2);
+
